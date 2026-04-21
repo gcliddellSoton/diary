2a02270bdfc587ed02c5e8663bb85a75c748908c
Geoffrey Liddell <g.c.liddell@soton.ac.uk>
2026-04-21 10:49:24 +0100

    2026-04-21 10-49-24

---
+++ scripts/open-obsidian.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VAULT = "~/Documents/Obsidian Vault"
+SYNC = "~/Documents/Obsidian Vault/scripts/obsidian-sync.sh"
+
+cd "$VAULT"
+echo "Obsidian Vault opening..."
+pwd
+
+# pull and open
+git pull --rebase
+echo "...changes pulled"
+flatpak run md.obsidian.Obsidian --vault="$VAULT"
+
+wait
+
+#push on close
+bash "$SYNC"
