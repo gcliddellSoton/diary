f163ee7963f6046dae96ac879a58b99e25c75a45
Geoffrey Liddell <g.c.liddell@soton.ac.uk>
2026-04-21 11:52:43 +0100

    2026-04-21 11:52:43

--- scripts/obsidian-sync.sh
+++ scripts/obsidian-sync.sh
@@ -7,7 +7,7 @@ pwd
 if ! git diff --quiet || git diff --cached --quiet || -n "$(git ls-files --others --exclude-standard)" ]; then
 	echo "changes detected, commit-pull-push..."
 	git add .
-	git commit -m "$(date '+%Y-%m-%d %H-%M-%S')"
+	git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 	echo "...commit successful."
 	git pull --rebase
 	git push
