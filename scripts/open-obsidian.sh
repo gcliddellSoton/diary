6fd6b545da90c6d06558f9a1c4368c635276532e
Geoffrey Liddell <g.c.liddell@soton.ac.uk>
2026-04-21 11:21:37 +0100

    2026-04-21 11-21-37

--- scripts/open-obsidian.sh
+++ scripts/open-obsidian.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-VAULT = "/Documents/Obsidian Vault"
-SYNC = "/Documents/Obsidian Vault/scripts/obsidian-sync.sh"
-
-cd "$VAULT"
-echo "Obsidian Vault opening..."
-pwd
-
-# pull and open
-git pull --rebase
-echo "...changes pulled"
-flatpak run md.obsidian.Obsidian --vault="$VAULT"
+# sync and open
+bash obsidian-sync.sh
+echo "...sync on open..."
+flatpak run md.obsidian.Obsidian --vault=.. &
 
 wait
 
 #push on close
-ls -al
+echo "...sync on close..."
 bash obsidian-sync.sh
