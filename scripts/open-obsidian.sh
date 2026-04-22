51bc2a8ce1c95f7d07a949a232ff038ab26be6c2
Geoffrey Liddell <g.c.liddell@soton.ac.uk>
2026-04-22 15:42:20 +0100

    2026-04-22 15:42:20

--- scripts/open-obsidian.sh
+++ scripts/open-obsidian.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-pwd
-cd "$(dirname "$0")"
-pwd
+cd "$(dirname "$0")" | exit 1
 
 # sync and open
 bash obsidian-sync.sh
-echo "...sync on open..."
+echo "sync on open... $(date '+%Y-%m-%d %H:%M:%S')" >> sync.log
 flatpak run md.obsidian.Obsidian --vault=.. &
 
 wait
 
 #push on close
-echo "...sync on close..."
+echo "sync on close... $(date '+%Y-%m-%d %H:%M:%S')" >> sync.log
 bash obsidian-sync.sh
